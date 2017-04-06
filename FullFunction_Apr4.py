acbabb7111230a2aed9c60e173daba76d06ea156
Schwenker <cwf29@nau.edu>
2017-04-06 00:38:57 -0700

    Update FullFunction_Apr4.py

--- FullFunction_Apr4.py
+++ FullFunction_Apr4.py
@@ -1,12 +1,15 @@
-## To resume after Apr 4th
-## Finish working on rounding the pointsCurrent_steps matrix
-## and feeding it to goto()
+## Updated: Apr 5, 2017
+## To do:
+## Add label to lie-angle buttons
+## Add button + entry-box for arbitrary lie-angles 
+## Add entry box to choose move distance per press of manual direction buttons
+## Add function to control the pendulum
 
 #import RPi.GPIO as gpio
 import time
 import sys
 import math
-#import numpy as np
+import numpy as np
 from functools import partial
 
 import tkinter
@@ -29,7 +32,7 @@ buttonsize_relative = 0.3
 gotogrid_offset_x = 55
 gotogrid_offset_y = 80
 # Logo gold = "#d4bc20"
-buttonColor = "#d48c20"
+buttonColor = "#ffd700"
 bColor_active = "#000000000"    #Pure black
 dotColor = ((0.0, 255.99609375, 0.0), '#00ff00')
 
@@ -84,99 +87,101 @@ directionPendLower = 1
 # Common lie angles at address are 15, 16, and 17 degrees
 # Row 1 is x coordinates (left to right)
 # Row 2 is y coordinates (top to bottom)
-##points0_mm = np.matrix('-20 -15 -10 -5 0 5 10 15 20 -20 -15 -10 -5 0 5 10 15 20 -20 -15 -10 -5 0 5 10 15 20 -20 -15 -10 -5 0 5 10 15 20 -20 -15 -10 -5 0 5 10 15 20; 10 10 10 10 10 10 10 10 10 5 5 5 5 5 5 5 5 5 0 0 0 0 0 0 0 0 0 -5 -5 -5 -5 -5 -5 -5 -5 -5 -10 -10 -10 -10 -10 -10 -10 -10 -10')
-##ang = math.radians(-15)
-##rotator = [[math.cos(ang), math.sin(ang)], [-(math.sin(ang)), math.cos(ang)]]
-##points15_mm = np.matmul(rotator, points0_mm)
-##ang = math.radians(-1)
-##rotator = [[math.cos(ang), math.sin(ang)],
-##                     [-(math.sin(ang)), math.cos(ang)]]
-##points16_mm = np.matmul(rotator, points15_mm)
-##points17_mm = np.matmul(rotator, points16_mm)
-##pointsOther_mm = points0_mm
+pts0_mm = np.matrix('-20 -15 -10 -5 0 5 10 15 20 -20 -15 -10 -5 0 5 10 15 20 -20 -15 -10 -5 0 5 10 15 20 -20 -15 -10 -5 0 5 10 15 20 -20 -15 -10 -5 0 5 10 15 20; 10 10 10 10 10 10 10 10 10 5 5 5 5 5 5 5 5 5 0 0 0 0 0 0 0 0 0 -5 -5 -5 -5 -5 -5 -5 -5 -5 -10 -10 -10 -10 -10 -10 -10 -10 -10')
+ang = math.radians(-15)
+rotator = [[math.cos(ang), math.sin(ang)], [-(math.sin(ang)), math.cos(ang)]]
+pts15_mm = np.matmul(rotator, pts0_mm)
+ang = math.radians(-1)
+rotator = [[math.cos(ang), math.sin(ang)],
+                     [-(math.sin(ang)), math.cos(ang)]]
+pts16_mm = np.matmul(rotator, pts15_mm)
+pts17_mm = np.matmul(rotator, pts16_mm)
+ptsother_mm = pts0_mm
 
-points15_steps = points15_mm
-points15_steps[0][:] *= mm2steps_horiz
-points15_steps[1][:] *= mm2steps_vert
+pts15_steps = pts15_mm
+pts15_steps[0][:] *= mm2steps_horiz
+pts15_steps[1][:] *= mm2steps_vert
+pts15_steps[:][:] = np.round(pts15_steps[:][:])
+pts15_steps = pts15_steps.astype(int)
 
-points16_steps = points16_mm
-points16_steps[0][:] *= mm2steps_horiz
-points16_steps[1][:] *= mm2steps_vert
- 
-points17_steps = points17_mm
-points17_steps[0][:] *= mm2steps_horiz
-points17_steps[1][:] *= mm2steps_vert
+pts16_steps = pts16_mm
+pts16_steps[0][:] *= mm2steps_horiz
+pts16_steps[1][:] *= mm2steps_vert
+pts16_steps[:][:] = np.round(pts16_steps[:][:])
+pts16_steps = pts16_steps.astype(int)
 
-pointsOther_steps = pointsOther_mm
-pointsOther_steps[0][:] *= mm2steps_horiz
-pointsOther_steps[1][:] *= mm2steps_vert
+pts17_steps = pts17_mm
+pts17_steps[0][:] *= mm2steps_horiz
+pts17_steps[1][:] *= mm2steps_vert
+pts17_steps[:][:] = np.round(pts17_steps[:][:])
+pts17_steps = pts17_steps.astype(int)
 
-pointsCurrent_steps = points15_steps
+ptsother_steps = ptsother_mm
+
+ptsCurrent_steps = pts15_steps
+cp_steps = np.matrix('0; 0') # Current position in steps
 
 def donothing():    # Do nothing
     print("Do nothing")
 
 def calibrate():
-    pos_steps_horiz = 0
-    pos_steps_vert = 0
-    print("Horizontal position: %s mm" % (pos_steps_horiz) )
-    print("Vertical position: %s mm" % (pos_steps_vert) )
+    cp_steps=np.matrix('0; 0')
+    print("Calibrated")
 
-##def rotate(angle):
-##    if angle==15:
-##        pointsCurrent_steps = points15_steps
-##        return points15_mm
-##    elif angle==16:
-##        pointsCurrent_steps = points16_steps
-##        return points16_mm
-##    elif angle==17:
-##        pointsCurrent_steps = points17_steps    
-##        return points17_mm
-##    else:
-##        ang = math.radians(-angle)
-##        rotator = np.matrix([[math.cos(ang), math.sin(ang)],
-##                         [-(math.sin(ang)), math.cos(ang)]])
-##        pointsOther_mm = np.matmul(rotator, points0_mm)
-##        pointsCurrent_steps = pointsOther_mm    
-##        return pointsOther_mm
-
-def goto(point):
-    horiz_dist =  pos_steps_horiz
-    print("Go to point %s" % (point) )
+def rotate(angle): # Positive angle is counterclockwise
+    print("Lie angle: %s deg" %angle)
+    if angle==15:
+        ptsCurrent_steps = pts15_steps
+    elif angle==16:
+        ptsCurrent_steps = pts16_steps
+    elif angle==17:
+        ptsCurrent_steps = pts17_steps
+    else:
+        ang = math.radians(-angle)
+        rotator = np.matrix([[math.cos(ang), math.sin(ang)],
+                         [-(math.sin(ang)), math.cos(ang)]])
+        ptsother_mm = np.matmul(rotator, pts0_mm)
+        ptsother_steps = ptsother_mm
+        ptsother_steps[0][:] *= mm2steps_horiz
+        ptsother_steps[1][:] *= mm2steps_vert
+        ptsother_steps[:][:] = np.round(pts17_steps[:][:])
+        ptsother_steps = ptsother_steps.astype(int)
+        ptsCurrent_steps = ptsother_steps   
 
 def stepLeft(dist=100):
+    cp_steps[0,0] += dist
+    if dist < 0:
+        dist *= -1      
 ##    gpio.output(pin_sleep, sleepOFF)
 ##    gpio.output(pin_directionHorizontal, directionLeft)
-##      for x in range(0,dist-1):
-##        #while(buttonHeld == True)
+##      for x in range(dist):
 ##            gpio.output(pin_stepHorizontal, 0)
 ##            time.sleep(sleeptime/2)
 ##            gpio.output(pin_stepHorizontal, 1)
 ##            time.sleep(sleeptime/2)
 ##    gpio.output(pin_sleep, sleepON)
-    print("Old horizontal position: %s" % (pos_steps_horiz) )
-    pos_steps_horiz = pos_step_horiz - dist
-    print("New horizontal position: %s" % (pos_steps_horiz) )
-    
 
-def stepRight(dist):
+def stepRight(dist=100):
+    cp_steps[0,0] += dist
+    if dist < 0:
+        dist *= -1
 ##    gpio.output(pin_sleep, sleepOFF)
 ##    gpio.output(pin_directionHorizontal, directionRight)
-##    for x in range(0,dist-1):
+##    for x in range(dist):
 ##            gpio.output(pin_stepHorizontal, 0)
 ##            time.sleep(sleeptime/2)
 ##            gpio.output(pin_stepHorizontal, 1)
 ##            time.sleep(sleeptime/2)
 ##    gpio.output(pin_sleep, sleepON)
-    print("Old horizontal position: %s" % (pos_steps_horiz) )
-    pos_steps_horiz = pos_step_horiz + dist
-    print("New horizontal position: %s" % (pos_steps_horiz) )
 
-def stepUp(dist):
+def stepUp(dist=100):
+    cp_steps[1,0] += dist
+    if dist < 0:
+        dist *= -1
 ##    gpio.output(pin_sleep, sleepOFF)
 ##    gpio.output(pin_directionVertical, directionUp)
 ##    gpio.output(pin_directionVerticalRight, directionUp)
-##    for x in range(0,dist-1):
+##    for x in range(dist):
 ##            gpio.output(pin_stepVertical, 0)
 ##            gpio.output(pin_stepVerticalRight, 0)
 ##            time.sleep(sleeptime/2)
@@ -184,27 +189,34 @@ def stepUp(dist):
 ##            gpio.output(pin_stepVerticalRight, 1)
 ##            time.sleep(sleeptime/2)
 ##    gpio.output(pin_sleep, sleepON)
-    print("Old vertical position: %s" % (pos_steps_vert) )
-    pos_steps_vert = pos_step_vert + dist
-    print("New horizontal position: %s" % (pos_steps_vert) )
 
-def stepDown(dist):
+def stepDown(dist=100):
+    cp_steps[1,0] += dist
+    if dist < 0:
+        dist *= -1
 ##    gpio.output(pin_sleep, sleepOFF)
 ##    gpio.output(pin_directionVertical, directionDown)
 ##    gpio.output(pin_directionVerticalRight, directionDown)
-##    for x in range(0,dist-1):
+##    for x in range(dist):
 ##            gpio.output(pin_stepVertical, 0)
 ##            gpio.output(pin_stepVerticalRight, 0)
 ##            time.sleep(sleeptime/2)
 ##            gpio.output(pin_stepVertical, 1)
 ##            gpio.output(pin_stepVerticalRight, 1)
 ##            time.sleep(sleeptime/2)
-##    gpio.output(pin_sleep,
-    print("Old vertical position: %s" % (pos_steps_vert) )
-    pos_steps_vert = pos_step_vert - dist
-    print("New horizontal position: %s" % (pos_steps_vert) )
-
+##    gpio.output(pin_sleep, sleepON)
     
+    
+def goto(point):
+    dist = np.subtract(ptsCurrent_steps[:,point], cp_steps)
+    if dist[0,0] < 0:
+        stepLeft(dist[0,0])
+    elif dist[0,0] > 0:
+        stepRight(dist[0,0])
+    if dist[1,0] < 0:
+        stepDown(dist[1,0])
+    elif dist[1,0] > 0:
+        stepUp(dist[1,0])  
 
 
 def manual():       # Open the manual controls mneu
@@ -243,20 +255,20 @@ def manual():       # Open the manual controls mneu
         for ctr in range(45):
             if ctr==22:
                 point_button = Button(manual_goto_menu, text="23\n(Center)", height=buttonheight, width=buttonwidth,
-                                     bg=buttonColor, activebackground=bColor_active, command=partial(goto, ctr+1))
+                                     bg=buttonColor, activebackground=bColor_active, command=partial(goto, ctr))
                 this_row, this_col = divmod(ctr,9)
                 point_button.grid(in_=manual_goto_frame, row=this_row, column=this_col)   
             else:
                 point_button = Button(manual_goto_menu, text=str(ctr+1), height=buttonheight, width=buttonwidth,
-                                      bg=buttonColor, activebackground=bColor_active, command=partial(goto, ctr+1))
+                                      bg=buttonColor, activebackground=bColor_active, command=partial(goto, ctr))
                 this_row, this_col = divmod(ctr,9)
                 point_button.grid(in_=manual_goto_frame, row=this_row, column=this_col)   
          
-
     # Back button to closethe manual control menu
     manual_backButton = tkinter.Button(manual_menu, text="<-", bg=buttonColor, activebackground=bColor_active, command=manual_back)
     manual_backButton.pack()
     manual_backButton.place(anchor=NW, relheight=buttonsize_relative/2, relwidth=buttonsize_relative/2)
+    ##########################################3
     # Left button in the manual control menu
     manual_leftButton = tkinter.Button(manual_menu, text = "Left", bg = buttonColor, activebackground=bColor_active, command=stepLeft)
     manual_leftButton.pack()
@@ -278,7 +290,7 @@ def manual():       # Open the manual controls mneu
     # Button to open the Go-to-a-point menu
     manual_gotoButton = tkinter.Button(manual_menu, text="Go to position", bg=buttonColor, activebackground=bColor_active, command = manual_goto)
     manual_gotoButton.pack()
-    manual_gotoButton.place(relx=(1-buttonsize_relative)/2, rely=(1-buttonsize_relative)/2+1.25*buttonsize_relative, relheight=buttonsize_relative/2, relwidth=buttonsize_relative)
+    manual_gotoButton.place(relx=(1-buttonsize_relative)/2, rely=(1-buttonsize_relative), relheight=buttonsize_relative/2, relwidth=buttonsize_relative)
     # Positional calibration button the manual control menu
     manual_calibrateButton = tkinter.Button(manual_menu, text = "Calibrate\nClub Position", bg=buttonColor, activebackground=bColor_active, command=manual_calibrate)
     manual_calibrateButton.pack()
@@ -304,18 +316,29 @@ def options():      # Open the options menu
         options_menu.destroy()
 
     # Back button in the options menu    
-    options_backButton = tkinter.Button(options_menu, text = "<-", bg=buttonColor, activebackground=bColor_active, command = options_back)
-    options_backButton.pack()
-    options_backButton.place(anchor=NW, relheight=buttonsize_relative/2, relwidth=buttonsize_relative/2)
+    options_backBtn = tkinter.Button(options_menu, text = "<-", bg=buttonColor, activebackground=bColor_active, command = options_back)
+    options_backBtn.pack()
+    options_backBtn.place(anchor=NW, relheight=buttonsize_relative/2, relwidth=buttonsize_relative/2)
     # Tracking color chooser button in the options menu
-    options_colorButton = tkinter.Button(options_menu, text = "Change Tracking Color", bg=buttonColor, activebackground=bColor_active, command = changeColor)
-    options_colorButton.pack()
-    options_colorButton.place(relx=(1-buttonsize_relative)/2, rely=(1-buttonsize_relative)/2, relheight=buttonsize_relative, relwidth=buttonsize_relative)
+##    options_colorBtn = tkinter.Button(options_menu, text = "Change Tracking Color", bg=buttonColor, activebackground=bColor_active, command = changeColor)
+##    options_colorBtn.pack()
+##    options_colorBtn.place(relx=(1-buttonsize_relative)/2, rely=(1-buttonsize_relative)/2, relheight=buttonsize_relative, relwidth=buttonsize_relative)
     # Position calibration button in the options menu
-    options_calibrateButton = tkinter.Button(options_menu, text = "Calibrate\nClub Position", bg=buttonColor, activebackground=bColor_active, command=options_calibrate)
-    options_calibrateButton.pack()
-    options_calibrateButton.place(relx=1-buttonsize_relative/2, rely=0, relheight=buttonsize_relative/2, relwidth=buttonsize_relative/2)
+    options_calibrateBtn = tkinter.Button(options_menu, text = "Calibrate\nClub Position", bg=buttonColor, activebackground=bColor_active, command=options_calibrate)
+    options_calibrateBtn.pack()
+    options_calibrateBtn.place(relx=(1-buttonsize_relative)/2, rely=0, relheight=buttonsize_relative, relwidth=buttonsize_relative)
+    # Radio buttons to select lie angle at address
+    options_pts15Btn = tkinter.Button(options_menu, text="15\nDegrees", bg=buttonColor, activebackground=bColor_active, command=rotate(15))
+    options_pts15Btn.pack()
+    options_pts15Btn.place(relx=(1-buttonsize_relative)/2-buttonsize_relative/4, rely=1-buttonsize_relative, relheight=buttonsize_relative, relwidth=buttonsize_relative/2)
+    options_pts16Btn = tkinter.Button(options_menu, text="16\nDegrees", bg=buttonColor, activebackground=bColor_active, command=rotate(16))
+    options_pts16Btn.pack()
+    options_pts16Btn.place(relx=(1-buttonsize_relative)/2+buttonsize_relative*1/4, rely=1-buttonsize_relative, relheight=buttonsize_relative, relwidth=buttonsize_relative/2)
+    options_pts17Btn = tkinter.Button(options_menu, text="17\nDegrees", bg=buttonColor, activebackground=bColor_active, command=rotate(17))
+    options_pts17Btn.pack()
+    options_pts17Btn.place(relx=(1-buttonsize_relative)/2+buttonsize_relative*3/4, rely=1-buttonsize_relative, relheight=buttonsize_relative, relwidth=buttonsize_relative/2)
 
+    
 
 # Begin main program here
 calibrate()
